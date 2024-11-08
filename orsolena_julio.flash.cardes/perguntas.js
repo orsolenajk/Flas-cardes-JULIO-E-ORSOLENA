fa6becf4f1a21e9cd4d977916fb03c4e4d5ab7d0
orsolenajk <134074470+orsolenajk@users.noreply.github.com>
2024-11-08 15:38:19 -0300

    Add files via upload

---
+++ orsolena_julio.flash.cardes/perguntas.js
@@ -0,0 +1,48 @@
+criaCartao(
+    'Itália',
+    'Qual é a capital da Itália?',
+    'Roma'
+)
+
+criaCartao(
+    'Itália',
+    'Qual a língua oficial da Itália?',
+    'Italiano'
+)
+
+criaCartao(
+    'Itália',
+    'Qual é o prato típico italiano conhecido mundialmente?',
+    'Pizza'
+)
+
+criaCartao(
+    'Itália',
+    'Qual é amoeda oficial da Itália?',
+    'Euro'
+)
+criaCartao(
+    'Itália',
+    'Qual é a cidade italiana conhecida pelos canais?',
+    'Veneza'
+)
+criaCartao(
+    'Itália',
+    'Qual é o famoso anfiteatro romana localizado em Roma?',
+    'Coliseu'
+)
+criaCartao(
+    'Itália',
+    'Qual é a maior ilha do Mediterrâneo pertencente na Itália?',
+    'Sicília'
+)
+criaCartao(
+    'Itália',
+    'Qual é o famoso festival de cinema realizado anualmente em Veneza?',
+    'Festival Internacional de Cinema de Veneza '
+)
+criaCartao(
+    'Itália',
+    'Qual é a principal religião pertencida na Itália?',
+    'Catolicismo'
+)
